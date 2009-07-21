35711746a1656a86523cb1f0866c39eedd513fd5
Dane Harrigan <dane.harrigan@gmail.com>
2009-07-21 00:27:33 -0400

    Updated README, added 'redo' feature to generator

--- generators/challenge_response/challenge_response_generator.rb
+++ generators/challenge_response/challenge_response_generator.rb
@@ -1,12 +1,19 @@
 class ChallengeResponseGenerator < Rails::Generator::Base
   def manifest
+    if ARGV.first == 'redo' and File.exists? "#{RAILS_ROOT}/vendor/plugins/challenge_response/challenge_questions.yml"
+      FileUtils.rm "#{RAILS_ROOT}/vendor/plugins/challenge_response/challenge_questions.yml"
+    end
+
     # create the migrate folder
     FileUtils.mkdir "#{RAILS_ROOT}/db/migrate" unless File.directory? "#{RAILS_ROOT}/db/migrate"
     timestamp = Time.now.strftime('%Y%m%d%H%M%S')
 
     # add migration file
     record do |m|
-      m.file 'challenge_response_migration.rb', "db/migrate/#{timestamp}_challenge_response_migration.rb"
+      if ARGV.first == 'redo'
+        m.file 'challenge_response_migration.rb', "db/migrate/#{timestamp}_challenge_response_migration.rb"
+      end
+
       m.template 'challenge_questions.rb', "vendor/plugins/challenge_response/challenge_questions.yml"
     end
   end
