c0b2cf22e3d6c569b74e8512a651838252160069
Dane Harrigan <dane.harrigan@gmail.com>
2009-07-22 00:01:33 -0400

    Added custom validate method to ChallengedSession

--- app/models/challenged_session.rb
+++ app/models/challenged_session.rb
@@ -1,11 +1,16 @@
 class ChallengedSession < ActiveRecord::Base
   validates_presence_of :challenge_id
   validates_numericality_of :challenge_id, :greater_than => 0
-  validate Proc.new { |c| c.challenge_id <= Challenge.count }
+  validate :valid_challenge_id
 
   validates_presence_of :session
   validates_length_of :session, :is => 44
   validates_format_of :session, :with => /\A[\w\d\+=]+\z/
 
   belongs_to :challenge
+
+  protected
+  def valid_challenge_id
+    errors.add(:challenge_id, 'Invalid challenge ID') if challenge_id > Challenge.count
+  end
 end
